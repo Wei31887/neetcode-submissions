8ec17d25473f7dcfe66b3ab5c721e9012bca341e
WeiTim <51775957+Wei31887@users.noreply.github.com>
2026-08-26 23:08:56 -0400

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if len(height) < 3:
+            return 0
+        
+        amt = 0
+        stack = []
+        for i in range(len(height)):
+            curH = height[i]
+            while stack and curH > height[stack[-1]]:
+
+                buttonIdx = stack.pop()
+                if not stack:
+                    continue
+                
+                leftIdx = stack[-1]
+                
+                area = (i - leftIdx - 1) * (min(height[leftIdx], curH) - height[buttonIdx])
+                amt += area
+
+            stack.append(i)
+        
+        return amt
